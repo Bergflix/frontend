24b9c48fe4cdb10a84c125ca5b2931135248d5fd
maximilian <info@apelma.de>
2020-06-25 11:04:33 +0200

    Small changes to User

--- src/classes/User.js
+++ src/classes/User.js
@@ -3,6 +3,11 @@ import PartySocket from "./PartySocket";
 
 class User {
     socket;
+    partySocket;
+
+    name;
+    email;
+
 
     constructor() {
         this.socket = io.connect("https://backend.bergflix.de");
@@ -14,7 +19,14 @@ class User {
     }
 
     login(){
-        this.socket.emit("login", {user: "Test", password: "Test"});
+        // Basic auth und Token auth sollten möglich sein. (Token auth für "angemeldet bleiben")
+        this.socket.emit("login", {
+            auth: "Basic",
+            data: {
+                name: "test@bergflix.de",
+                password: "geheim"
+            }
+        });
     }
 }
 
