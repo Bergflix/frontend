998045ccb232844db676b124443db61293c4c20e
RyanFlm <info@apelma.de>
2020-10-18 19:31:07 +0200

    Firebase integration in the client

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style.scss';
 import App from './App';
+import './style.scss';
+
 import * as serviceWorker from './serviceWorker';
 
+import * as firebase from "firebase/app";
+import "firebase/analytics";
+
+// Init Firebase
+firebase.initializeApp({
+    apiKey: "AIzaSyDjyqf2udEWXrqq315403hjHN4DFiSVsEg",
+    authDomain: "bergflix.firebaseapp.com",
+    databaseURL: "https://bergflix.firebaseio.com",
+    projectId: "bergflix",
+    storageBucket: "bergflix.appspot.com",
+    messagingSenderId: "916166332578",
+    appId: "1:916166332578:web:c19c17ba0472d5963707fb",
+    measurementId: "G-9VRVMYV8CW"
+});
+firebase.analytics();
+
+// Render ReactApp
 ReactDOM.render(<App />, document.getElementById('root'));
 
+// Register ServiceWorker
 serviceWorker.register();
