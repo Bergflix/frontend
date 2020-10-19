7997551a8fc5fe5a7c874bf57a5faf7adda3bcb1
LeXonJe <30697846+LeXonJe@users.noreply.github.com>
2020-10-19 12:30:13 +0200

    Added 'Bergflix. Original' Logo and added arrow icon

---
+++ src/components/Elements/BOLogo.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './style.scss';
+
+class BOLogo extends Component {
+    render() {
+        return <span className={'bo-logo'}>Bergflix<span>. Original</span></span>;
+    }
+}
+
+export default BOLogo;
