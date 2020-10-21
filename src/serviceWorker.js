df400e013802909421e8f7f4224c190db4148f1f
RyanFlm <info@apelma.de>
2020-10-21 22:44:58 +0200

    Just a cleanup I guess

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,70 +1,41 @@
-const isLocalhost = Boolean(
-  window.location.hostname === 'localhost'
-    || window.location.hostname === '[::1]'
-    || window.location.hostname.match(
-      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-    )
-);
+const isLocalhost = ["localhost", "[::1]"].includes(window.location.hostname) || window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/);
 
 export function register(config) {
-  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+    if(process.env.NODE_ENV !== 'production' || !'serviceWorker' in navigator) return;
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
-    if (publicUrl.origin !== window.location.origin) {
-      return;
-    }
+    if (publicUrl.origin !== window.location.origin) return;
 
-    window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/offline.js`;
+    window.addEventListener('load', async () => {
+        const swUrl = `${process.env.PUBLIC_URL}/offline.js`;
 
-      if (isLocalhost) {
-        checkValidServiceWorker(swUrl, config);
-        navigator.serviceWorker.ready.then(() => {});
-      } else {
-        registerValidSW(swUrl, config);
-      }
+        if(isLocalhost) {
+            await checkValidServiceWorker(swUrl, config);
+            await navigator.serviceWorker.ready;
+        } else await registerValidSW(swUrl, config);
     });
-  }
 }
 
-function checkValidServiceWorker(swUrl, config) {
-  fetch(swUrl, {
-    headers: { 'Service-Worker': 'script' }
-  }).then(response => {
+async function checkValidServiceWorker(swUrl, config) {
+    let response = await fetch(swUrl, {headers: { 'Service-Worker': 'script' }});
     const contentType = response.headers.get('content-type');
+
     if (response.status === 404 || (contentType != null && contentType.indexOf('javascript') === -1)) {
-      navigator.serviceWorker.ready.then(registration => {
-        registration.unregister().then(() => {
-          window.location.reload();
-        });
-      });
-    } else {
-      registerValidSW(swUrl, config);
-    }
-  }).catch(() => {});
+        let registration = await navigator.serviceWorker.ready;
+        await registration.unregister();
+        window.location.reload();
+    } else await registerValidSW(swUrl, config);
 }
 
-function registerValidSW(swUrl, config) {
-  navigator.serviceWorker.register(swUrl).then(registration => {
+async function registerValidSW(swUrl, config) {
+    let registration = await navigator.serviceWorker.register(swUrl);
     registration.onupdatefound = () => {
-      const installingWorker = registration.installing;
-      if (installingWorker == null) {
-        return;
-      }
+        const installingWorker = registration.installing;
+        if(installingWorker == null) return;
 
-      installingWorker.onstatechange = () => {
-        if (installingWorker.state === 'installed') {
-          if (navigator.serviceWorker.controller) {
-            if (config && config.onUpdate) {
-              config.onUpdate(registration);
-            }
-          }else{
-            if(config && config.onSuccess) {
-              config.onSuccess(registration);
-            }
-          }
-        }
-      };
+        installingWorker.onstatechange = () => {
+            if(installingWorker.state !== 'installed') return;
+            if(navigator.serviceWorker.controller) config && config.onUpdate && config.onUpdate(registration);
+            else config && config.onSuccess && config.onSuccess(registration);
+        };
     };
-  })
-  .catch(() => {});
 }
