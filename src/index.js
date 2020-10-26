1cbedd135ac84be0b03791fc979bb452c49af13d
RyanFlm <info@apelma.de>
2020-10-26 21:36:30 +0100

    Revoke firebaseApp and create new with .env

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './style.scss';
-
-import * as serviceWorker from './serviceWorker';
 
 import * as firebase from "firebase/app";
 import "firebase/performance";
 import "firebase/analytics";
 
+import App from './App';
+import './style.scss';
+import * as serviceWorker from './serviceWorker';
+
+console.log(process.env);
 // Init Firebase
 firebase.initializeApp({
-    apiKey: "AIzaSyDjyqf2udEWXrqq315403hjHN4DFiSVsEg",
-    authDomain: "bergflix.firebaseapp.com",
-    databaseURL: "https://bergflix.firebaseio.com",
-    projectId: "bergflix",
-    storageBucket: "bergflix.appspot.com",
-    messagingSenderId: "916166332578",
-    appId: "1:916166332578:web:c19c17ba0472d5963707fb",
-    measurementId: "G-9VRVMYV8CW"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
 });
 // Init performance monitoring
 firebase.performance();
