ea148dac32e7acd90ce14d16dd431a5cced1699e
LeXonJe <30697846+LeXonJe@users.noreply.github.com>
2020-10-26 20:45:47 +0100

    Added TabContainer Component

---
+++ src/components/TabContainer/index.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+import './style.scss';
+
+class TabContainer extends Component {
+    state = {
+        activeChild: this.props.children[0],
+    };
+
+    onTabClick(tab) {
+        this.setState({ activeChild: tab });
+    }
+
+    render() {
+        return (
+            <div className={'tab-container' + ((this.props.className && ' ' + this.props.className) || '')}>
+                <div className={'tab-header'}>
+                    {this.props.children.map((child) => {
+                        return (
+                            <span className={'tab' + (child === this.state.activeChild ? ' tab-active' : '')} onClick={() => this.onTabClick(child)}>
+                                {child.props.label}
+                            </span>
+                        );
+                    })}
+                </div>
+                <div className={'tab-content'}>
+                    {this.props.children.map((child) => {
+                        if (child === this.state.activeChild) return child.props.children;
+                        return undefined;
+                    })}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default TabContainer;
