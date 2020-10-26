1043f4a6bffa0e0d62519557150a6665833eedc1
LeXonJe <30697846+LeXonJe@users.noreply.github.com>
2020-10-26 20:45:19 +0100

    Changed link to /media/:key

--- src/components/ElementList/index.js
+++ src/components/ElementList/index.js
@@ -1,21 +1,20 @@
-import React from "react";
-import "./style.scss";
-import { Link } from "react-router-dom";
+import React from 'react';
+import './style.scss';
+import { Link } from 'react-router-dom';
 
 class ElementList extends React.Component {
-
     render() {
         if (!this.props.list) return;
 
         return (
-            <div id={"list-container"}>
-                <div className={"list"}>
-                    {this.props.list.map(item => (
-                        <Link key={item.id} className={"list-item"} to={`/watch/${item.id}`}>
-                            <img className={"item-image"} alt={"Thumbnail"} src={item.thumbnail} />
-                            <span className={"item-title"}>{item.title}</span>
-                        </Link>)
-                    )}
+            <div id={'list-container'}>
+                <div className={'list'}>
+                    {this.props.list.map((item) => (
+                        <Link key={item.id} className={'list-item'} to={`/media/${item.id}`}>
+                            <img className={'item-image'} alt={'Thumbnail'} src={item.thumbnail} />
+                            <span className={'item-title'}>{item.title}</span>
+                        </Link>
+                    ))}
                 </div>
             </div>
         );
