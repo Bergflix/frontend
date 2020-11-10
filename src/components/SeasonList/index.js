43e349d25f50d29c7368ab0928c471a474dae4ee
LeXonJe <30697846+LeXonJe@users.noreply.github.com>
2020-11-10 20:36:47 +0100

    Changed whole appearance of seasonlist

--- src/components/SeasonList/index.js
+++ src/components/SeasonList/index.js
@@ -3,81 +3,35 @@ import { Link } from 'react-router-dom';
 
 import './style.scss';
 
-import Icon from '../Elements/Icon';
-
-let changeToMobile = 800;
-
 class NavigationList extends Component {
-    state = {
-        page: this.props.page || 0,
-        mobileView: window.innerWidth < changeToMobile + 1,
-    };
-
-    /*
-    componentDidMount() {
-        window.addEventListener('resize', () => this.handleResize());
-    }
-
-    handleResize() {
-        let nowView = window.innerWidth < changeToMobile + 1;
-        console.log(this.state.mobileView + ' ' + nowView);
-        if (this.state.mobileView !== nowView) {
-            console.log('resize');
-            this.setState({ mobileView: nowView });
-        }
-    }
-    */
-
-    onNext() {
-        this.setState({ page: this.state.page + 1 });
-    }
-
-    onBack() {
-        this.setState({ page: this.state.page - 1 });
-    }
+    state = {};
 
     render() {
-        let serieId = this.props.serieId;
-        let content = [];
-        let renderNav = false;
-
-        let partIndex = this.state.page * 5;
-        let renderBackButton = this.state.page * 5 > 0;
-        let renderNextButton = this.state.page * 5 + 5 < this.props.content.length;
-
-        if (this.state.mobileView) {
-            partIndex = 0;
-            renderBackButton = false;
-            renderNextButton = false;
-            content = this.props.content;
-        } else {
-            if (this.props.content.length > 5) {
-                for (let i = partIndex; i < this.state.page * 5 + 5; i++) {
-                    content.push(this.props.content[i]);
-                }
-                renderNav = true;
-            } else {
-                content = this.props.content;
-            }
-        }
+        let partIndex = 0;
 
         return (
             <div className={'season-container'}>
-                {renderNav ? <Icon type={'back'} className={renderBackButton ? 'nav-button' : 'nav-button-deactivated'} onClick={() => this.onBack()} /> : undefined}
-                <div className={'nav-list'}>
-                    {content.map((part) => {
-                        return (
-                            <Link key={part.ytid} className={'part'} to={`/watch/${serieId}/${part.ytid}`}>
-                                <img className={'thumbnail'} src={part.thumbnail} alt={'Thumbnail'} />
+                {this.props.content.map((part) => {
+                    return (
+                        <Link key={part.ytid} className={'part'} to={`/watch/${this.props.serieId}/${part.ytid}`}>
+                            <span className={'part-preview'}>
+                                <img className={'part-thumbnail'} src={part.thumbnail} alt={'Thumbnail'} />
                                 <span className={'watch-indicator'} style={{ width: `${65}%` } /*TODO: Add length calculation*/} />
-                                <span className={'title'}>
+                            </span>
+                            <span className={'part-info'}>
+                                <span className={'part-title'}>
                                     {++partIndex}. {part.title}
                                 </span>
-                            </Link>
-                        );
-                    })}
-                </div>
-                {renderNav ? <Icon type={'next'} className={renderNextButton ? 'nav-button' : 'nav-button-deactivated'} onClick={() => this.onNext()} /> : undefined}
+                                <p className={'part-description'}>Voluptatem quos ratione et perspiciatis et quos molestiae. Sint reprehenderit eum earum natus eius cupiditate itaque. Doloremque ab dolores nihil nihil. Molestias quo ipsum dicta. Aperiam est aut in enim nostrum velit eos odio.</p>
+                                <span className={'part-tags'}>
+                                    <span>25min</span>
+                                    <span />
+                                    <span>11.12.2019</span>
+                                </span>
+                            </span>
+                        </Link>
+                    );
+                })}
             </div>
         );
     }
