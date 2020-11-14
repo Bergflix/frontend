b1314ade510b83efbe53ba7b832499b9de50df32
LeXonJe <30697846+LeXonJe@users.noreply.github.com>
2020-11-14 18:13:58 +0100

    Added select menu for mobile

--- src/components/TabContainer/index.js
+++ src/components/TabContainer/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 import './style.scss';
 
+import Icon from '../Elements/Icon';
+
 class TabContainer extends Component {
     state = {
         activeChild: this.props.children[0],
@@ -15,13 +17,24 @@ class TabContainer extends Component {
         return (
             <div className={'tab-container' + ((this.props.className && ' ' + this.props.className) || '')}>
                 <div className={'tab-header'}>
-                    {this.props.children.map((child) => {
-                        return (
-                            <span key={child.props.label} className={'tab' + (child === this.state.activeChild ? ' tab-active' : '')} onClick={() => this.onTabClick(child)}>
-                                {child.props.label}
-                            </span>
-                        );
-                    })}
+                    <span className={'tab-header-list'}>
+                        {this.props.children.map((child) => {
+                            return (
+                                <span key={child.props.label} className={'tab' + (child === this.state.activeChild ? ' tab-active' : '')} onClick={() => this.onTabClick(child)}>
+                                    {child.props.label}
+                                </span>
+                            );
+                        })}
+                    </span>
+                    <select className={'tab-header-select'}>
+                        {this.props.children.map((child) => {
+                            return (
+                                <option key={child.props.label} onClick={() => this.onTabClick(child)}>
+                                    {child.props.label}
+                                </option>
+                            );
+                        })}
+                    </select>
                 </div>
                 <div className={'tab-content'}>
                     {this.props.children.map((child) => {
