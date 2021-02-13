2e0dfb5f8264774a58070656c4c015e7c0f3e568
RyanFlm <info@apelma.de>
2021-02-13 18:46:47 +0100

    Change background

--- src/pages/Info/index.js
+++ src/pages/Info/index.js
@@ -55,50 +55,52 @@ class Info extends Component {
         }
 
         return (
-            <div id={'info-container'} className={renderSeasons ? 'serie-align' : 'movie-align'}>
-                <Helmet>
-                    <title>
-                        Bergflix - {renderedType} - {title}
-                    </title>
-                    <meta name={'description'} content={`Bergflix Startseite. Neuerscheinung: ${title}`} />
-                </Helmet>
-                <img className={'logo'} src={logo} alt={'Element Logo'} onError={(e) => (e.target.outerHTML = `<p class="logo">${title}</p>`)} />
-                <div className={'info'}>
-                    <span>{year}</span>
-                    <span>{age}+</span>
-                    <span>{renderedType}</span>
-                    <span />
-                    <span>{genre}</span>
-                </div>
-                <div className={'info-2'}>
-                    <BOLogo />
-                    {/*TODO: Make an if statement*/}
-                </div>
-                <div className={'description'}>{description}</div>
-                {!renderSeasons && (
-                    <div className={'timeline'}>
+            <>
+                <div id={'info-container'} className={renderSeasons ? 'serie-align' : 'movie-align'}>
+                    <Helmet>
+                        <title>
+                            Bergflix - {renderedType} - {title}
+                        </title>
+                        <meta name={'description'} content={`Bergflix Startseite. Neuerscheinung: ${title}`} />
+                    </Helmet>
+                    <img className={'logo'} src={logo} alt={'Element Logo'} onError={(e) => (e.target.outerHTML = `<p class="logo">${title}</p>`)} />
+                    <div className={'info'}>
+                        <span>{year}</span>
+                        <span>{age}+</span>
+                        <span>{renderedType}</span>
                         <span />
-                        <span>150min</span>
+                        <span>{genre}</span>
                     </div>
-                )}
-                <div className={'controls'}>
-                    {hasBegun ? (
-                        <React.Fragment>
+                    <div className={'info-2'}>
+                        <BOLogo />
+                        {/*TODO: Make an if statement*/}
+                    </div>
+                    <div className={'description'}>{description}</div>
+                    {!renderSeasons && (
+                        <div className={'timeline'}>
+                            <span />
+                            <span>150min</span>
+                        </div>
+                    )}
+                    <div className={'controls'}>
+                        {hasBegun ? (
+                            <React.Fragment>
+                                <Link to={`/watch/${_id}`}>
+                                    <Icon type={'play'} />
+                                    <span>Weiterschauen</span>
+                                </Link>
+                                <Link to={`/watch/${_id}`}>
+                                    <Icon type={'refresh'} />
+                                    <span>Neustarten</span>
+                                </Link>
+                            </React.Fragment>
+                        ) : (
                             <Link to={`/watch/${_id}`}>
                                 <Icon type={'play'} />
-                                <span>Weiterschauen</span>
+                                <span>Abspielen</span>
                             </Link>
-                            <Link to={`/watch/${_id}`}>
-                                <Icon type={'refresh'} />
-                                <span>Neustarten</span>
-                            </Link>
-                        </React.Fragment>
-                    ) : (
-                        <Link to={`/watch/${_id}`}>
-                            <Icon type={'play'} />
-                            <span>Abspielen</span>
-                        </Link>
-                    )}
+                        )}
+                    </div>
                 </div>
                 {renderSeasons && (
                     <TabContainer className={'seasons'}>
@@ -111,7 +113,7 @@ class Info extends Component {
                         })}
                     </TabContainer>
                 )}
-            </div>
+            </>
         );
     }
 }
